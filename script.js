2612c2f78465793932c9f640cb62fe4174eed3f5
Simon Kinch <simon.kinch@digitalleadership.edu.je>
2021-11-13 10:52:41 +0000

    get game up to a working share trading game, albeit with lots of cumbersome, repeated code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let userBalance = 1000;
 
+let season = 1994;
+
 let sharesArsenal = 0;
 let sharesAstonVilla = 0;
 let sharesBlackburn = 0;
@@ -10,6 +12,17 @@ let sharesForest = 0;
 let sharesQPR = 0;
 let sharesTottenham = 0;
 let sharesWimbledon = 0;
+
+const historyArsenal = 130;
+const historyAstonVilla = 120;
+const historyBlackburn = 145;
+const historyLiverpool = 135;
+const historyManUtd = 150;
+const historyNewcastle = 140;
+const historyForest = 125;
+const historyQPR = 110;
+const historyTottenham = 115;
+const historyWimbledon = 105;
 
 let priceArsenal = 130;
 let priceAstonVilla = 120;
@@ -67,149 +80,198 @@ document.querySelector('#priceWimbledon').innerHTML = `£${priceWimbledon}`;
 document.querySelector('#lastDvndWimbledon').innerHTML = `£${lastDvndWimbledon}`;
 
 function buyArsenal() {
+    if (userBalance >= priceArsenal) {
     sharesArsenal += 1;
     userBalance -= priceArsenal;
     document.querySelector('#sharesArsenal').innerHTML = `${sharesArsenal}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellArsenal() {
+    if (sharesArsenal > 0) {
     sharesArsenal -= 1;
     userBalance += priceArsenal;
     document.querySelector('#sharesArsenal').innerHTML = `${sharesArsenal}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyAstonVilla() {
+    if (userBalance >= priceAstonVilla) {
     sharesAstonVilla += 1;
     userBalance -= priceAstonVilla;
     document.querySelector('#sharesAstonVilla').innerHTML = `${sharesAstonVilla}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellAstonVilla() {
+    if (sharesAstonVilla > 0) {
     sharesAstonVilla -= 1;
     userBalance += priceAstonVilla;
     document.querySelector('#sharesAstonVilla').innerHTML = `${sharesAstonVilla}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyBlackburn() {
+    if (userBalance >= priceBlackburn) {
     sharesBlackburn += 1;
     userBalance -= priceBlackburn;
     document.querySelector('#sharesBlackburn').innerHTML = `${sharesBlackburn}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellBlackburn() {
+    if (sharesBlackburn > 0) {
     sharesBlackburn -= 1;
     userBalance += priceBlackburn;
     document.querySelector('#sharesBlackburn').innerHTML = `${sharesBlackburn}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyLiverpool() {
+    if (userBalance >= priceLiverpool) {
     sharesLiverpool += 1;
     userBalance -= priceLiverpool;
     document.querySelector('#sharesLiverpool').innerHTML = `${sharesLiverpool}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellLiverpool() {
+    if (sharesLiverpool > 0) {
     sharesLiverpool -= 1;
     userBalance += priceLiverpool;
     document.querySelector('#sharesLiverpool').innerHTML = `${sharesLiverpool}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyManUtd() {
+    if (userBalance >= priceManUtd) {
     sharesManUtd += 1;
     userBalance -= priceManUtd;
     document.querySelector('#sharesManUtd').innerHTML = `${sharesManUtd}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellManUtd() {
+    if (sharesManUtd > 0) {
     sharesManUtd -= 1;
     userBalance += priceManUtd;
     document.querySelector('#sharesManUtd').innerHTML = `${sharesManUtd}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyNewcastle() {
+    if (userBalance >= priceNewcastle) {
     sharesNewcastle += 1;
     userBalance -= priceNewcastle;
     document.querySelector('#sharesNewcastle').innerHTML = `${sharesNewcastle}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellNewcastle() {
+    if (sharesNewcastle > 0) {
     sharesNewcastle -= 1;
     userBalance += priceNewcastle;
     document.querySelector('#sharesNewcastle').innerHTML = `${sharesNewcastle}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyForest() {
+    if (userBalance >= priceForest) {
     sharesForest += 1;
     userBalance -= priceForest;
     document.querySelector('#sharesForest').innerHTML = `${sharesForest}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellForest() {
+    if (sharesForest > 0) {
     sharesForest -= 1;
     userBalance += priceForest;
     document.querySelector('#sharesForest').innerHTML = `${sharesForest}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyQPR() {
+    if (userBalance >= priceQPR) {
     sharesQPR += 1;
     userBalance -= priceQPR;
     document.querySelector('#sharesQPR').innerHTML = `${sharesQPR}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellQPR() {
+    if (sharesQPR > 0) {
     sharesQPR -= 1;
     userBalance += priceQPR;
     document.querySelector('#sharesQPR').innerHTML = `${sharesQPR}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyTottenham() {
+    if (userBalance >= priceTottenham) {
     sharesTottenham += 1;
     userBalance -= priceTottenham;
     document.querySelector('#sharesTottenham').innerHTML = `${sharesTottenham}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellTottenham() {
+    if (sharesTottenham > 0) {
     sharesTottenham -= 1;
     userBalance += priceTottenham;
     document.querySelector('#sharesTottenham').innerHTML = `${sharesTottenham}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function buyWimbledon() {
+    if (userBalance >= priceWimbledon) {
     sharesWimbledon += 1;
     userBalance -= priceWimbledon;
     document.querySelector('#sharesWimbledon').innerHTML = `${sharesWimbledon}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
 function sellWimbledon() {
+    if (sharesWimbledon > 0) {
     sharesWimbledon -= 1;
     userBalance += priceWimbledon;
     document.querySelector('#sharesWimbledon').innerHTML = `${sharesWimbledon}`;
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
+    }
 }
 
-
+function runSeason() {
+    if (userBalance < 25) {
+        alert("Insufficient funds - sell some shares!");
+    } else {
+        nextSeason();
+    }
+}
 
 function nextSeason() {
+    
     userBalance -= 25;
+
+    season += 1;
 
     let randomArsenal = ((100 + (Math.floor(Math.random() * 11) - 5) + (Math.floor(Math.random() * 11) - 5)) / 100);
     if ( randomArsenal < 1) {
@@ -217,7 +279,7 @@ function nextSeason() {
     } else {
         lastDvndArsenal = ((priceArsenal / 20) * randomArsenal);
     }
-    priceArsenal = (priceArsenal * randomArsenal);
+    priceArsenal = ((priceArsenal * randomArsenal) + historyArsenal)/2;
     earnArsenal = (sharesArsenal * lastDvndArsenal);
     userBalance += earnArsenal;
     document.querySelector('#dvndArsenal').innerHTML = `£${lastDvndArsenal.toFixed(2)}`;
@@ -231,7 +293,7 @@ function nextSeason() {
     } else {
         lastDvndAstonVilla = ((priceAstonVilla / 20) * randomAstonVilla);
     }
-    priceAstonVilla = (priceAstonVilla * randomAstonVilla);
+    priceAstonVilla = ((priceAstonVilla * randomAstonVilla) + historyAstonVilla)/2;
     earnAstonVilla = (sharesAstonVilla * lastDvndAstonVilla);
     userBalance += earnAstonVilla;
     document.querySelector('#dvndAstonVilla').innerHTML = `£${lastDvndAstonVilla.toFixed(2)}`;
@@ -245,7 +307,7 @@ function nextSeason() {
     } else {
         lastDvndBlackburn = ((priceBlackburn / 20) * randomBlackburn);
     }
-    priceBlackburn = (priceBlackburn * randomBlackburn);
+    priceBlackburn = ((priceBlackburn * randomBlackburn) + historyBlackburn)/2;
     earnBlackburn = (sharesBlackburn * lastDvndBlackburn);
     userBalance += earnBlackburn;
     document.querySelector('#dvndBlackburn').innerHTML = `£${lastDvndBlackburn.toFixed(2)}`;
@@ -259,7 +321,7 @@ function nextSeason() {
     } else {
         lastDvndLiverpool = ((priceLiverpool / 20) * randomLiverpool);
     }
-    priceLiverpool = (priceLiverpool * randomLiverpool);
+    priceLiverpool = ((priceLiverpool * randomLiverpool) + historyLiverpool)/2;
     earnLiverpool = (sharesLiverpool * lastDvndLiverpool);
     userBalance += earnLiverpool;
     document.querySelector('#dvndLiverpool').innerHTML = `£${lastDvndLiverpool.toFixed(2)}`;
@@ -273,7 +335,7 @@ function nextSeason() {
     } else {
         lastDvndManUtd = ((priceManUtd / 20) * randomManUtd);
     }
-    priceManUtd = (priceManUtd * randomManUtd);
+    priceManUtd = ((priceManUtd * randomManUtd) + historyManUtd)/2;
     earnManUtd = (sharesManUtd * lastDvndManUtd);
     userBalance += earnManUtd;
     document.querySelector('#dvndManUtd').innerHTML = `£${lastDvndManUtd.toFixed(2)}`;
@@ -287,7 +349,7 @@ function nextSeason() {
     } else {
         lastDvndNewcastle = ((priceNewcastle / 20) * randomNewcastle);
     }
-    priceNewcastle = (priceNewcastle * randomNewcastle);
+    priceNewcastle = ((priceNewcastle * randomNewcastle) + historyNewcastle)/2;
     earnNewcastle = (sharesNewcastle * lastDvndNewcastle);
     userBalance += earnNewcastle;
     document.querySelector('#dvndNewcastle').innerHTML = `£${lastDvndNewcastle.toFixed(2)}`;
@@ -301,7 +363,7 @@ function nextSeason() {
     } else {
         lastDvndForest = ((priceForest / 20) * randomForest);
     }
-    priceForest = (priceForest * randomForest);
+    priceForest = ((priceForest * randomForest) + historyForest)/2;
     earnForest = (sharesForest * lastDvndForest);
     userBalance += earnForest;
     document.querySelector('#dvndForest').innerHTML = `£${lastDvndForest.toFixed(2)}`;
@@ -315,7 +377,7 @@ function nextSeason() {
     } else {
         lastDvndQPR = ((priceQPR / 20) * randomQPR);
     }
-    priceQPR = (priceQPR * randomQPR);
+    priceQPR = ((priceQPR * randomQPR) + historyQPR)/2;
     earnQPR = (sharesQPR * lastDvndQPR);
     userBalance += earnQPR;
     document.querySelector('#dvndQPR').innerHTML = `£${lastDvndQPR.toFixed(2)}`;
@@ -329,7 +391,7 @@ function nextSeason() {
     } else {
         lastDvndTottenham = ((priceTottenham / 20) * randomTottenham);
     }
-    priceTottenham = (priceTottenham * randomTottenham);
+    priceTottenham = ((priceTottenham * randomTottenham) + historyTottenham)/2;
     earnTottenham = (sharesTottenham * lastDvndTottenham);
     userBalance += earnTottenham;
     document.querySelector('#dvndTottenham').innerHTML = `£${lastDvndTottenham.toFixed(2)}`;
@@ -343,7 +405,7 @@ function nextSeason() {
     } else {
         lastDvndWimbledon = ((priceWimbledon / 20) * randomWimbledon);
     }
-    priceWimbledon = (priceWimbledon * randomWimbledon);
+    priceWimbledon = ((priceWimbledon * randomWimbledon) + historyWimbledon)/2;
     earnWimbledon = (sharesWimbledon * lastDvndWimbledon);
     userBalance += earnWimbledon;
     document.querySelector('#dvndWimbledon').innerHTML = `£${lastDvndWimbledon.toFixed(2)}`;
@@ -351,6 +413,7 @@ function nextSeason() {
     document.querySelector('#lastDvndWimbledon').innerHTML = `£${lastDvndWimbledon.toFixed(2)}`;
     document.querySelector('#earnWimbledon').innerHTML = `£${earnWimbledon.toFixed(2)}`;
 
+    document.querySelector('#seasonName').innerHTML = `${season}/${season + 1}`
 
     document.querySelector('#userBalance').innerHTML = `£${userBalance.toFixed(2)}`;
 }
